0ed29f1e564caedabd08ae9a8eeee067208d3613
Yasser Illanes <yass.illanes@duocuc.cl>
2025-09-29 15:31:00 -0300

    fix: Update react-router dependency to version 7.9.3 and refactor routing in App component

--- Vite-TiendaMiMascota/src/App.jsx
+++ Vite-TiendaMiMascota/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Router } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,11 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <App />
+        <Router>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Router>
       </BrowserRouter>
     </>
   )
